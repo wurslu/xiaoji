304d66bd871fe2aecf086c5d2fd9a950bb75da61
wuronglu <2923239110@qq.com>
2025-05-25 18:20:16 +0800

    更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 3
-        versionName = "3.0.1"
+        versionName = "3.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
